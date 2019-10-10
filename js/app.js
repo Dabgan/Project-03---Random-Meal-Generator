87dbc0c3c85581eff71092b271bc4bc5944026d7
dabgan <gabriel.daniluk.work@gmail.com>
2019-10-10 20:21:46 +0200

    Final commit

--- js/app.js
+++ js/app.js
@@ -18,69 +18,67 @@ $(function(){
     })
 
     function createMeal(res){
+        
+        const ingredients = [];
 
-        function getRes(response){
-           return JSON.stringify(response);
+        // Get all ingredients from the object. Up to 20
+        for (let i = 1; i <= 20; i++) {
+            if (res[`strIngredient${i}`]) {
+                ingredients.push(
+                    `${res[`strIngredient${i}`]} - ${res[`strMeasure${i}`]}`
+                );
+            } else {
+                break;
+            }
         }
 
         receipeContainer.html(`
-                                <div class="row">
-                                                    
-                                <div class="col-lg-6 border">
-                                    </p>Category: ${res.strCategory}</p>
-                                    </p>Area: ${res.strArea}</p>
-                                    ${res.strTags === null ? '' : `<p>Tags: ${res.strTags}</p>`}
-                                    <h3>Ingrediens:</h3>
-                                    <ul>
-                                        <li>${res.strMeasure1} ${res.strIngredient1}</li>
-                                        <li>${res.strMeasure2} ${res.strIngredient2}</li>
-                                        <li>${res.strMeasure3} ${res.strIngredient3}</li>
-                                        <li>${res.strMeasure4} ${res.strIngredient4}</li>
-                                        <li>${res.strMeasure5} ${res.strIngredient5}</li>
-                                        <li>${res.strMeasure6} ${res.strIngredient6}</li>
-                                        <li>${res.strMeasure7} ${res.strIngredient7}</li>
-                                        <li>${res.strMeasure8} ${res.strIngredient8}</li>
-                                        <li>${res.strMeasure9} ${res.strIngredient9}</li>
-                                        <li>${res.strMeasure10} ${res.strIngredient10}</li>
-                                        <li>${res.strMeasure11} ${res.strIngredient11}</li>
-                                        <li>${res.strMeasure12} ${res.strIngredient12}</li>
-                                        <li>${res.strMeasure13} ${res.strIngredient13}</li>
-                                        <li>${res.strMeasure14} ${res.strIngredient14}</li>
-                                        <li>${res.strMeasure15} ${res.strIngredient15}</li>
-                                        <li>${res.strMeasure16} ${res.strIngredient16}</li>
-                                        <li>${res.strMeasure17} ${res.strIngredient17}</li>
-                                        <li>${res.strMeasure18} ${res.strIngredient18}</li>
-                                        <li>${res.strMeasure19} ${res.strIngredient19}</li>
-                                        <li>${res.strMeasure20} ${res.strIngredient20}</li>
-                                    </ul>
+                            <div class="row border mb-4">             
+                            
+                                <div class="col-lg-6">
+                                    <div class="row">
+
+                                        <div class="col-lg-12 my-4 ">
+                                            <img class="w-100 mealThumb" src="${res.strMealThumb}" alt="">
+                                        </div>
+                                
+                                        <div class="col-lg-8">
+                                            <h3 class="mt-2">Ingrediens:</h3>
+                                            <ul class="list-group list-group-flush">
+                                                ${ingredients.map( ingredient => `<li class="text-lg-left list-item ml-3">${ingredient}</li>`).join('')}
+                                            </ul>
+                                        </div>
+
+                                        <div class="col-lg-4 ">
+                                            <h3 class="text-center mt-2">Category:</h3>
+                                            <p>${res.strCategory}</p>
+
+                                            <h3 class="text-center">Area:</h3>
+                                            <p> ${res.strArea}</p>
+
+                                            ${res.strTags === null ? '' : `<h3 class="text-center">Tags:</h3>
+                                            <p>${res.strTags.split(',').join(', ')}</p>`}
+                                        </div>  
+
+                                    </div>
                                 </div>
 
-                                <div class="col-lg-6 border">
-                                    <h2 class="h1">${res.strMeal}</h1>
-                                    <div class="">
-                                        <img class="w-100" src="${res.strMealThumb}" alt="">
+                                <div class="col-lg-6">
+                            
+                                    <h3 class="h1 mt-3">${res.strMeal}</h3>
+                            
+                                    <div class="col-lg-12 pt-3">
+                                        <p class="text-justify">${res.strInstructions}</p>
                                     </div>
+                            
+                                    <iframe class="my-3" width="100%" height="315"
+                                    src="https://www.youtube.com/embed/${res.strYoutube.slice(-11)}"
+                                    frameborder="0">
+                                    </iframe>
                                     
                                 </div>
-                            
-                                <div class="col-lg-12 border">
-                                    <h2 class="h1">INSTRUKCJA</h2>
-                                </div>
-                                <div class="col-lg-12 border">
-                                    <p>${res.strInstructions}</p>
-                                </div>
 
-                                <div class="col-lg-12 border">
-                                    <h2 class="h1">Video</h2>
-                                    <div class="bla">
-                                        <iframe width="420" height="315"
-                                        src="https://www.youtube.com/embed/${res.strYoutube.slice(-11)}"
-                                        frameborder="0">
-                                        </iframe>
-                                    </div>
-                                </div>
-                            
-                            </div>  
+                            </div>
             `);
     };
     
